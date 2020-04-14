528a4cb818acf83ddd4f59e5d4af622e3db7583d
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-14 13:35:36 -0700

    :shirt: Prettier lint

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,13 @@
-import React, {useState} from "react"
+import React, { useState } from 'react'
 import { Badge, Box, Button, Flex, Heading, Link, Text } from '@chakra-ui/core'
 
 const SIDE_PADDING = 4
 
-const SidebarSection = ({children}) => (<Box as="section" mb={6}>{children}</Box>)
+const SidebarSection = ({ children }) => (
+  <Box as="section" mb={6}>
+    {children}
+  </Box>
+)
 
 const SidebarCategory = ({ text }) => (
   <Text
@@ -33,16 +37,21 @@ const SidebarHeading = ({ href, text }) => (
 )
 
 const SidebarSubheading = ({ href, text, level = 1 }) => {
-    const horizontalPadding = Math.round(24 * (0.5 + (0.5 * level)))
-    return (
-      <Link href={href} px={`${horizontalPadding}px`} py={1} fontSize={14} display="block">
-        {text}
-      </Link>
-    )
+  const horizontalPadding = Math.round(24 * (0.5 + 0.5 * level))
+  return (
+    <Link
+      href={href}
+      px={`${horizontalPadding}px`}
+      py={1}
+      fontSize={14}
+      display="block"
+    >
+      {text}
+    </Link>
+  )
 }
 
 const Sidebar = () => {
-
   const [hidden, setHidden] = useState(false)
 
   const toggleSidebar = (e) => {
@@ -51,75 +60,86 @@ const Sidebar = () => {
     setHidden((prevState) => !prevState)
   }
 
-    const overflowSidebar = hidden ? 'visible' : 'scroll'
-    const hideMenu = hidden ? '-120%' : '0'
-    const menuHeight = hidden ? '3em' : '100vh'
+  const overflowSidebar = hidden ? 'visible' : 'scroll'
+  const hideMenu = hidden ? '-120%' : '0'
+  const menuHeight = hidden ? '3em' : '100vh'
 
-    return (
-      <Box
-        width={['100%', '300px']}
-        height={[menuHeight, '100vh']}
-        minHeight={['auto', '100vh']}
-        maxHeight={['auto', '100vh']}
-        bg="gray.50"
-        borderRightWidth="1px"
+  return (
+    <Box
+      width={['100%', '300px']}
+      height={[menuHeight, '100vh']}
+      minHeight={['auto', '100vh']}
+      maxHeight={['auto', '100vh']}
+      bg="gray.50"
+      borderRightWidth="1px"
+      borderColor="gray.40"
+      overflowY={[overflowSidebar, 'scroll']}
+      position="fixed"
+      zIndex={999}
+    >
+      <Flex
+        p={[1, SIDE_PADDING]}
+        mb={6}
+        justifyContent="space-between"
+        borderBottomWidth="1px"
         borderColor="gray.40"
-        overflowY={[overflowSidebar, 'scroll']}
-        position="fixed"
-        zIndex={999}
+        bg="gray.50"
+        position="sticky"
+        zIndex={710}
       >
-        <Flex
-          p={[1, SIDE_PADDING]}
-          mb={6}
-          justifyContent="space-between"
-          borderBottomWidth="1px"
-          borderColor="gray.40"
-          bg="gray.50"
-          position="sticky"
-          zIndex={710}
-        >
-          <Heading as="h1">Logo</Heading>
-          <Box>
-            <Badge>v1.0.0</Badge>
-            <Box display="inline-block" visibility={['visible', 'hidden']} marginLeft={4}><Button onClick={toggleSidebar}>Menu</Button></Box>
+        <Heading as="h1">Logo</Heading>
+        <Box>
+          <Badge>v1.0.0</Badge>
+          <Box
+            display="inline-block"
+            visibility={['visible', 'hidden']}
+            marginLeft={4}
+          >
+            <Button onClick={toggleSidebar}>Menu</Button>
           </Box>
-        </Flex>
-        <Box as="nav" zIndex={420} transition="transform 400ms ease-in" transform={[`translateX(${hideMenu})`, `translateX(0)`]}>
-          <SidebarSection>
-            <SidebarCategory text="API" />
-            <SidebarHeading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" level={2} />
-          </SidebarSection>
-          <SidebarSection>
-            <SidebarCategory text="API" />
-            <SidebarHeading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" level={2} />
-          </SidebarSection>
-          <SidebarSection>
-            <SidebarCategory text="API" />
-            <SidebarHeading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" level={2} />
-          </SidebarSection>
-          <SidebarSection>
-            <SidebarCategory text="API" />
-            <SidebarHeading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" />
-            <SidebarSubheading href="#" text="Getting Started" level={2} />
-          </SidebarSection>
         </Box>
+      </Flex>
+      <Box
+        as="nav"
+        zIndex={420}
+        transition="transform 400ms ease-in"
+        transform={[`translateX(${hideMenu})`, `translateX(0)`]}
+      >
+        <SidebarSection>
+          <SidebarCategory text="API" />
+          <SidebarHeading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" level={2} />
+        </SidebarSection>
+        <SidebarSection>
+          <SidebarCategory text="API" />
+          <SidebarHeading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" level={2} />
+        </SidebarSection>
+        <SidebarSection>
+          <SidebarCategory text="API" />
+          <SidebarHeading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" level={2} />
+        </SidebarSection>
+        <SidebarSection>
+          <SidebarCategory text="API" />
+          <SidebarHeading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" />
+          <SidebarSubheading href="#" text="Getting Started" level={2} />
+        </SidebarSection>
       </Box>
-    )
+    </Box>
+  )
 }
 
-Sidebar.defaultProps ={
-    hidden: false
+Sidebar.defaultProps = {
+  hidden: false,
 }
 
 export default Sidebar
--- layouts/DocsContent.jsx
+++ layouts/DocsContent.jsx
@@ -5,14 +5,16 @@ import Sidebar from '../components/Sidebar'
 export default function DocsContent({ children }) {
   // Width of Sidebar (300px) + 32px for padding
   // Accomodates for sidebar expanded in desktop
-  const SIDEBAR_WIDTH = "332px"
+  const SIDEBAR_WIDTH = '332px'
   return (
     <Flex as="main" flexDirection={['column', 'row']}>
       <Sidebar />
-      <Box paddingLeft={[10, SIDEBAR_WIDTH]} paddingRight={[10, 16]} py={[20, 10]}>
-        <Box minWidth={["100%", "640px"]}>
-          {children}
-        </Box>
+      <Box
+        paddingLeft={[10, SIDEBAR_WIDTH]}
+        paddingRight={[10, 16]}
+        py={[20, 10]}
+      >
+        <Box minWidth={['100%', '640px']}>{children}</Box>
       </Box>
     </Flex>
   )
